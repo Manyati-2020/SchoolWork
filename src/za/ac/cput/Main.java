347d1b5d230f6b71f65800f54215696d9cf297c1
Manyati-2020 <manymunya@gmail.com>
2020-04-08 18:31:43 +0200

    Initial commit

---
+++ src/za/ac/cput/Main.java
@@ -0,0 +1,10 @@
+package za.ac.cput;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        System.out.println("Joel 2020 Hello World ");
+    }
+}
